1cc8644072bfc4871118b378f6c903b37dfc8cbe
Randy Lai <randy.cs.lai@gmail.com>
2018-06-15 13:43:42 -0400

    add themes

--- theme.py
+++ theme.py
@@ -2,21 +2,57 @@ import sublime
 import sublime_plugin
 
 import os
+import json
 
 from .tools.theme_generator import generate_theme_file
 
 
+THEMES = os.path.join(os.path.dirname(__file__), "themes")
+
+
+class ConsoleSelectTheme(sublime_plugin.WindowCommand):
+    def run(self, theme=None):
+        if theme:
+            if theme not in ["default", "user"]:
+                themefile = os.path.join(THEMES, "{}.json".format(theme))
+                if not os.path.isfile(themefile):
+                    raise IOError("{} not found".format(themefile))
+
+            settings = sublime.load_settings("Console.sublime-settings")
+            settings.set("theme", theme)
+            sublime.save_settings("Console.sublime-settings")
+
+        else:
+            self.themes = ["default", "user"] + \
+                sorted([f.replace(".json", "") for f in os.listdir(THEMES) if f.endswith(".json")])
+            self.window.show_quick_panel(
+                self.themes,
+                self.on_selection)
+
+    def on_selection(self, index):
+        theme = self.themes[index]
+        self.window.run_command("console_select_theme", {"theme": theme})
+
+
 class ConsoleGenerateTheme(sublime_plugin.WindowCommand):
-    def run(self, remove=False):
+    def run(self, theme=None, remove=False):
         settings = sublime.load_settings("Console.sublime-settings")
 
-        theme = settings.get("theme", "default")
+        if not theme:
+            theme = settings.get("theme", "default")
         if theme == "user":
             variables = settings.get("user_theme_colors", {})
         elif theme == "default":
-            variables = settings.get("default_theme_colors", {})
-        else:
+            # variables = settings.get("default_theme_colors", {})
             variables = {}
+        else:
+            themefile = os.path.join(THEMES, "{}.json".format(theme))
+            if not os.path.isfile(themefile):
+                raise IOError("{} not found".format(themefile))
+
+            with open(themefile, "r") as f:
+                theme_data = json.load(f)
+                variables = theme_data["theme_colors"]
 
         path = os.path.join(
             sublime.packages_path(),
@@ -49,17 +85,19 @@ def plugin_loaded():
         if not os.path.isfile(path):
             sublime.active_window().run_command("console_generate_theme")
 
-    _cached_theme = settings.get("theme", "default")
-    _cached_user_theme_colors = settings.get("user_theme_colors", {}).copy()
+    _cached = {
+        "theme": settings.get("theme", "default"),
+        "user_theme_colors": settings.get("user_theme_colors", {}).copy()
+    }
 
     def on_change():
-        nonlocal _cached_theme, _cached_user_theme_colors
         theme = settings.get("theme", "default")
         user_theme_colors = settings.get("user_theme_colors", {}).copy()
-        if theme != _cached_theme or user_theme_colors != _cached_user_theme_colors:
+        if theme != _cached["theme"] or user_theme_colors != _cached["user_theme_colors"]:
+            print("will generate")
             sublime.active_window().run_command("console_generate_theme")
-            _cached_user_theme_colors = user_theme_colors
-            _cached_theme = theme
+            _cached["theme"] = theme
+            _cached["user_theme_colors"] = user_theme_colors
 
     settings.clear_on_change("user_theme_colors")
     settings.add_on_change("user_theme_colors", on_change)
