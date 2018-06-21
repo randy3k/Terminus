fb611859da7de0983a937b74dbf889d6b8259d64
Randy Lai <randy.cs.lai@gmail.com>
2018-06-21 17:06:44 -0400

    better edit keybindings listener

--- settings.py
+++ settings.py
@@ -2,15 +2,75 @@ import sublime
 import sublime_plugin
 
 
-class ConsoleEditSettingsListener(sublime_plugin.EventListener):
+class ConsoleEditKeybindingsListener(sublime_plugin.EventListener):
     def on_post_window_command(self, window, command, args):
         if command == "edit_settings":
             base = args.get("base_file", "")
             if base.endswith("sublime-keymap") and "/Console/Default" in base:
                 w = sublime.active_window()
+                user_view = w.active_view()
+                user_view.settings().erase("edit_settings_view")
+                user_view.settings().set("console_edit_keybindings_view", 'user')
                 w.focus_group(0)
+                base_platform_view = w.active_view()
+                base_platform_view.settings().erase("edit_settings_view")
+                base_platform_view.settings().set("console_edit_keybindings_view", 'base')
                 w.run_command("open_file", {"file": "${packages}/Console/Default.sublime-keymap"})
+                base_view = w.active_view()
+                base_view.settings().set("console_edit_keybindings_view", 'base')
                 w.focus_group(1)
+
+    def on_pre_close(self, view):
+        """
+        Grabs the window id before the view is actually removed
+        """
+        view_settings = view.settings()
+
+        if view_settings.get('console_edit_keybindings_view') is None:
+            return
+
+        if view.window() is None:
+            return
+
+        view.settings().set('window_id', view.window().id())
+
+    def on_close(self, view):
+        """
+        Closes the other settings view when one of the two is closed
+        """
+
+        view_settings = view.settings()
+
+        if view_settings.get('console_edit_keybindings_view') is None:
+            return
+
+        window_id = view_settings.get('window_id')
+        window = None
+        for win in sublime.windows():
+            if win.id() == window_id:
+                window = win
+                break
+
+        if not window:
+            return
+
+        views = window.views()
+        for other in views:
+            if other.settings().get("console_edit_keybindings_view"):
+                def _close():
+                    window.focus_view(other)
+                    # Prevent the handler from running on the other view
+                    other.settings().erase('edit_settings_view')
+                    window.run_command("close")
+
+                # Run after timeout so the UI doesn't block with the view half closed
+                sublime.set_timeout(_close)
+
+        if len(window.views()) == 0 and len(window.folders()) < 1:
+            def close_window():
+                if window.id() == sublime.active_window().id():
+                    window.run_command("close_window")
+            sublime.set_timeout(close_window, 50)
 
 
 class ConsoleEditSettingsCommand(sublime_plugin.WindowCommand):
