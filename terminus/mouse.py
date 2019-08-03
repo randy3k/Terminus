97f74ad2ceee77bd3742c630540411b2939e45b7
MaxST <mstolpasov@gmail.com>
2019-08-04 01:54:48 +0300

    Is this possibly useful? I made opportunity open a file in the current view from terminus for example Traceback python: ` Traceback (most recent call last):   File "/home/user/.pyenv/versions/3.7.4/lib/python3.7/runpy.py", line 193, in _run_module_as_main     "__main__", mod_spec)   File "/home/user/.pyenv/versions/3.7.4/lib/python3.7/runpy.py", line 85, in _run_code     exec(code, run_globals)   File "client/__main__.py", line 13, in <module>     from module import settings ModuleNotFoundError: No module named 'module' `

--- terminus/mouse.py
+++ terminus/mouse.py
@@ -1,17 +1,18 @@
+# -*- coding: utf-8 -*-
+import logging
+import re
+import webbrowser
+from pathlib import Path
+
 import sublime
 import sublime_plugin
 
-import re
-import logging
-import webbrowser
-
-from .terminal import Terminal, CONTINUATION
+from .terminal import CONTINUATION, Terminal
 from .utils import highlight_key
 
 logger = logging.getLogger('Terminus')
 
-rex = re.compile(
-    r'''(?x)
+rex = re.compile(r'''(?x)
     \b(?:
         https?://(?:(?:[a-zA-Z0-9\-_]+(?:\.[a-zA-Z0-9\-._]+)+)|localhost)|  # http://
         www\.[a-zA-Z0-9\-_]+(?:\.[a-zA-Z0-9\-._]+)+                         # www.
@@ -19,6 +20,7 @@ rex = re.compile(
     /?[a-zA-Z0-9\-._?,!'(){}\[\]/+&@%$#=:"|~;]*                             # url path and query string
     [a-zA-Z0-9\-_~:/#@$*+=]                                                 # allowed end chars
     ''')
+rexf = re.compile(r'(?i)(?:(?<=\"))(?P<file>[a-zA-Z0-9\-_\./\\]+)(?:(?=\"))(?:\",)?(?:\sline\s)(?P<line>[0-9]+)')
 
 URL_POPUP = """
 <style>
@@ -41,30 +43,7 @@ div {
 """
 
 
-def find_url(view, event=None, pt=None):
-    if event:
-        pt = view.window_to_text((event["x"], event["y"]))
-    line = view.line(pt)
-
-    line.a = max(line.a, pt - 1024)
-    line.b = pt + 1024
-
-    text = view.substr(line)
-    text = text.replace(CONTINUATION + "\n", "")
-    it = rex.finditer(text)
-
-    for match in it:
-        if match.start() <= (pt - line.a) and match.end() >= (pt - line.a):
-            url = text[match.start():match.end()]
-            if url[0:3] == "www":
-                return "http://" + url
-            else:
-                return url
-
-    return None
-
-
-def find_url_region(view, event=None, pt=None):
+def find_common(view, event=None, pt=None):
     if event:
         pt = view.window_to_text((event["x"], event["y"]))
     line = view.line(pt)
@@ -75,8 +54,49 @@ def find_url_region(view, event=None, pt=None):
     text = view.substr(line)
     original_text = text
     text = text.replace(CONTINUATION + "\n", "")
+    return text, line, pt, original_text
 
+
+def find_url(view, event=None, pt=None):
+    text, line, pt, _ = find_common(view, event, pt)
     for match in rex.finditer(text):
+        if match.start() <= (pt - line.a) and match.end() >= (pt - line.a):
+            url = text[match.start():match.end()]
+            if url[0:3] == "www":
+                return "http://" + url
+            else:
+                return url
+    return None
+
+
+def find_file(view, event=None, pt=None):
+    text, line, pt, _ = find_common(view, event, pt)
+    for m in rexf.finditer(text):
+        return '%s:%s' % (m.group('file'), m.group('line'))
+    return None
+
+
+def find_something(view, event=None, pt=None):
+    thing = find_url(view, event, pt)
+    if thing:
+        return thing, 'url'
+    thing = find_file(view, event, pt)
+    if thing:
+        path, line = thing.split(':')
+        full_path = path = Path(path)
+        if not path.is_absolute():
+            for folder in view.window().folders():
+                full_path = Path(folder).joinpath(path)
+                if full_path.exists():
+                    break
+        if full_path.exists():
+            return '%s:%s' % (full_path, line), 'file'
+    return (None, None)
+
+
+def find_url_region(view, event=None, pt=None, reg=rex):
+    text, line, pt, original_text = find_common(view, event, pt)
+    for match in reg.finditer(text):
         if match.start() <= (pt - line.a) and match.end() >= (pt - line.a):
             a = match.start()
             b = match.end()
@@ -88,6 +108,8 @@ def find_url_region(view, event=None, pt=None):
 
 
 class TerminusMouseEventListener(sublime_plugin.EventListener):
+
+    type_reg = {'url': rex, 'file': rexf}
 
     def on_text_command(self, view, command_name, args):
         terminal = Terminal.from_id(view.id())
@@ -103,20 +125,31 @@ class TerminusMouseEventListener(sublime_plugin.EventListener):
             return
         if hover_zone != sublime.HOVER_TEXT:
             return
-        url = find_url(view, pt=point)
+        for_open, type_ = find_something(view, pt=point)
 
-        if not url:
+        if not for_open:
             return
 
         def on_navigate(action):
             if action == "open":
-                webbrowser.open_new_tab(url)
+                webbrowser.open_new_tab(for_open)
+
+        def on_navigate_file(action):
+            if action == "open":
+                view.window().open_file(for_open, sublime.ENCODED_POSITION)
 
         def on_hide():
             if link_key:
                 view.erase_regions(link_key)
 
-        url_region = find_url_region(view, pt=point)
+        regexp = self.type_reg[type_]
+        if type_ == 'url':
+            on_nav = on_navigate
+        else:
+            on_nav = on_navigate_file
+
+        url_region = find_url_region(view, reg=regexp, pt=point)
+
         link_key = None
         if url_region:
             link_key = highlight_key(view)
@@ -124,19 +157,27 @@ class TerminusMouseEventListener(sublime_plugin.EventListener):
                 link_key,
                 [sublime.Region(*url_region)],
                 "meta",
-                flags=sublime.DRAW_NO_FILL | sublime.DRAW_NO_OUTLINE | sublime.DRAW_SOLID_UNDERLINE)
+                flags=sublime.DRAW_NO_FILL
+                | sublime.DRAW_NO_OUTLINE
+                | sublime.DRAW_SOLID_UNDERLINE,
+            )
 
         view.show_popup(
             URL_POPUP,
             sublime.HIDE_ON_MOUSE_MOVE_AWAY,
             location=point,
-            on_navigate=on_navigate, on_hide=on_hide)
+            on_navigate=on_nav,
+            on_hide=on_hide,
+        )
 
 
 class TerminusOpenContextUrlCommand(sublime_plugin.TextCommand):
     def run(self, edit, event):
-        url = find_url(self.view, event)
-        webbrowser.open_new_tab(url)
+        for_open, type_ = find_something(self.view, event)
+        if type_ == 'url':
+            webbrowser.open_new_tab(for_open)
+        elif type_ == 'file':
+            self.view.window().open_file(for_open, sublime.ENCODED_POSITION)
 
     def is_enable(self, *args, **kwargs):
         terminal = Terminal.from_id(self.view.id())
@@ -144,13 +185,13 @@ class TerminusOpenContextUrlCommand(sublime_plugin.TextCommand):
 
     def is_visible(self, event):
         terminal = Terminal.from_id(self.view.id())
-        return terminal is not None and find_url(self.view, event) is not None
+        return terminal is not None and find_something(self.view, event)[0] is not None
 
     def description(self, event):
-        url = find_url(self.view, event)
-        if len(url) > 64:
-            url = url[0:64] + "..."
-        return "Open " + url
+        for_open, type_ = find_something(self.view, event)
+        if len(for_open) > 64:
+            for_open = for_open[0:64] + "..."
+        return "Open " + for_open
 
     def want_event(self):
         return True
