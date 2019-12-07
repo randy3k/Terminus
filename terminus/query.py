a29ee922fef15a23904a35ca8b20959b7aab1d61
Randy Lai <randy.cs.lai@gmail.com>
2019-12-06 19:01:36 -0800

    helper to show terminus build results

--- terminus/query.py
+++ terminus/query.py
@@ -1,6 +1,8 @@
 import sublime
 import sublime_plugin
 
+from .core import EXEC_PANEL
+
 
 class TerminusQueryContextListener(sublime_plugin.EventListener):
 
@@ -10,7 +12,23 @@ class TerminusQueryContextListener(sublime_plugin.EventListener):
     """
 
     def on_query_context(self, view, key, operator, operand, match_all):
-        if key.startswith("terminus_view"):
+        if key == "terminus_view.exec_panel_exists":
+            if not view.window():
+                return
+            exists = view.window().find_output_panel(EXEC_PANEL) is not None
+            if exists == operand and operator == sublime.OP_EQUAL:
+                return True
+            elif exists != operand and operator == sublime.OP_NOT_EQUAL:
+                return True
+        elif key == "terminus_view.exec_panel_visible":
+            if not view.window():
+                return
+            visible = view.window().active_panel() == "output.{}".format(EXEC_PANEL)
+            if visible == operand and operator == sublime.OP_EQUAL:
+                return True
+            elif visible != operand and operator == sublime.OP_NOT_EQUAL:
+                return True
+        elif key.startswith("terminus_view"):
             tag = view.settings().get(key, None)
             if tag == operand and operator == sublime.OP_EQUAL:
                 return True
