92e72a316436071e13aed3d9eb468859cc1170f8
Randy Lai <randy.cs.lai@gmail.com>
2021-06-21 16:38:27 -0700

    fix settings_on_change

--- terminus/utils.py
+++ terminus/utils.py
@@ -101,29 +101,38 @@ def rev_wcwidth(text, width):
         return i + width - w
 
 
-def settings_on_change(settings, keys, clear=True):
+def settings_on_change(settings, keys, clear=False):
     if not isinstance(keys, list):
+        singleton = True
         keys = [keys]
+    else:
+        singleton = False
     _cached = {}
     for key in keys:
         _cached[key] = settings.get(key, None)
 
-    def on_change_factory(key, on_change):
-        def _():
+    _key = "terminus_{}_{}".format(settings.settings_id, ".".join(keys))
+
+    if clear:
+        settings.clear_on_change(_key)
+        return
+
+    def check_cache_values(on_change):
+        run_on_change = False
+
+        for key in keys:
             value = settings.get(key)
             if _cached[key] != value:
-                try:
-                    on_change(value)
-                finally:
-                    _cached[key] = value
+                _cached[key] = value
+                run_on_change = True
 
-        return _
+        if run_on_change:
+            if singleton and len(_cached) == 1:
+                on_change(list(_cached.values())[0])
+            else:
+                on_change(_cached)
 
     def _(on_change):
-        for key in keys:
-            if clear:
-                settings.clear_on_change(key)
-
-            settings.add_on_change(key, on_change_factory(key, on_change))
+        settings.add_on_change(_key, lambda: check_cache_values(on_change))
 
     return _
